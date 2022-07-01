3d84293c96371b280a80c0c5399a2db566a3d0d4
Sergii Kamenskyi <serg@apple.com>
2022-07-02 01:47:23 +0300

    added PostgreSQL environment variable and fix in requirement.txt

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 
@@ -6,7 +8,8 @@ from resources.fact import Fact
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pi:$ecretP2nda@192.168.0.112:5432/pi'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pi:$ecretP2nda@192.168.0.112:5432/pi'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mjmonarch'
 api = Api(app)
