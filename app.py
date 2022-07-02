2156467bb71bb6ec6fbfc19c0970b7b55f04151c
Sergii Kamenskyi <serg@apple.com>
2022-07-02 10:20:25 +0300

    fixed error with auto db creating via sqlalchemy

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ from resources.fact import Fact
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pi:$ecretP2nda@192.168.0.112:5432/pi'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pi:$ecretP2nda@192.168.0.112:5432/pi'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mjmonarch'
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+     db.create_all(app=app)
 
 routes = ['/facts', '/facts/<string:vrp_no>']
 api.add_resource(Fact, *routes)
+
 
 
 if __name__ == '__main__':
