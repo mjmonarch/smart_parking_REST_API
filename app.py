8c94154da7fddc601028971f38cbfdc3cb13e6cf
Sergii Kamenskyi <serg@apple.com>
2022-07-02 11:29:37 +0300

    trying to fix auto db creation

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from db import db
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pi:$ecretP2nda@192.168.0.112:5432/pi'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_C_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mjmonarch'
 api = Api(app)
