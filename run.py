1065dfdfb6b98c332e454541925563f681673b63
Sergii Kamenskyi <serg@apple.com>
2022-07-09 19:18:53 +0300

    Admin row in users db hardcoded

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
+import base64
+
 from app import app
 from db import db
+from models.user import UserModel
 
 db.init_app(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all(app=app)
+
+    admin = UserModel('admin', base64.b64encode('strong_password'.encode("utf-8")))
+    db.session.add(admin)
+    db.session.commit()
+
+    
